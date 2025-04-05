1112dcb00dc4b0c513aa175e1ec2bb6f16df1105
fujitaka08 <121796462+fujitaka08@users.noreply.github.com>
2025-04-06 00:24:49 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/p_circuit_proto/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0x3e, 0x2d, 0x09, 0x8b, 0x2a, 0x8c, 0x32, 0x2a}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
